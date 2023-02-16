bd85e04aee98fed92a00227621b3a85c10b77214
Leonardo Puchetta <rissottona@gmail.com>
2023-02-15 21:26:42 -0300

    Cambios para deploy

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,2 +1,3 @@
-export const basePath = "http://localhost:3977/api";
+// export const basePath = "http://localhost:3977/api";
+export const basePath = "https://portfolio-server-2175.onrender.com/api";
 export const apiVersion = "v1";
