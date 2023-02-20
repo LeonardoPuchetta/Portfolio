3a22b39a2547378ffe0be2b407d9548a68af705b
Leonardo Puchetta <rissottona@gmail.com>
2023-02-20 20:17:04 -0300

    cambios en loading

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -6,9 +6,12 @@ import FoxColorIcon from './../assets/icons/fox-1.svg'
 export default function Loading() {
   return (
     <div className='loading'>
-        <div className='loader'> 
+        <div className='loading-image'> 
             <img src={FoxColorIcon} alt='loadImage'/>
          </div>
+        <div className='loading-text'>
+              Un poco de paciencia por favor ...
+        </div>
     </div>
   )
 }
